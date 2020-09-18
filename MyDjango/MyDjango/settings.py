5d89f7f68ef976090da9abcdee09bd150c732320
chen <781055847@qq.com>
2020-09-18 22:37:00 +0800

    ORM

--- MyDjango/MyDjango/settings.py
+++ MyDjango/MyDjango/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'MyDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "kecheng",
+        "USER": "root",
+        "PASSWORD": "123456",
+        "HOST": "192.168.80.128",
+        "PORT":"3319"
     }
 }
 
--- MyDjango/yoyo/models.py
+++ MyDjango/yoyo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PeronInfo(models.Model):
+
+    name=models.CharField(max_length=30)
+    age=models.IntegerField(null=True,blank=True)
+
+class chengji(models.Model):
+    uid=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30)
+    age=models.IntegerField()
